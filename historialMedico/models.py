fd64f9dbcfcfd7d11a3a0d4d10a1774393d426c9
Matias Molins <137366337+MolinsMatias@users.noreply.github.com>
2025-07-08 16:45:23 -0400

    Update models.py

--- historialMedico/models.py
+++ historialMedico/models.py
@@ -1,19 +1,25 @@
 from django.db import models
 from paciente.models import Paciente
-from medico.models import Medico  # Asegúrate de que este import apunte al modelo correcto
+from medico.models import Medico 
+from django.db.models import Max
 
 class HistorialMedico(models.Model):
-    id_hist = models.BigAutoField(primary_key=True)  # NUMBER(38) con NOT NULL, asumido como autoincremental
-    fec_creacion = models.DateField()  # DATE
-    est_visib = models.IntegerField()  # NUMBER
-    observ = models.CharField(max_length=500)  # VARCHAR2(500)
-    
+    id_hist = models.AutoField(primary_key=True)
+    fec_creacion = models.DateField()
+    est_visib = models.IntegerField()
+    observ = models.CharField(max_length=500)
+
     paciente_medico = models.ForeignKey(Medico, on_delete=models.CASCADE, db_column='PACIENTE_MEDICO_ID_MED')
     paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, db_column='PACIENTE_ID_PAC')
 
-
     class Meta:
-        db_table = 'HIST_MEDICO'  # Puedes cambiarlo al nombre real si es distinto
+        db_table = 'HIST_MEDICO'
 
     def __str__(self):
         return f'Historial #{self.id_hist} - Paciente {self.paciente_id}'
+
+    def save(self, *args, **kwargs):
+        if self.id_hist is None:
+            ultimo_id = HistorialMedico.objects.aggregate(Max('id_hist'))['id_hist__max']
+            self.id_hist = (ultimo_id or 0) + 1
+        super().save(*args, **kwargs)
